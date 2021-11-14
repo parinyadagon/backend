53ade5a238b22c7eb84edf83912200cc10a03fdc
parinya <dagonparinya@gmail.com>
2021-11-14 12:55:04 +0700

    .

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express By Parinya 5555' });
 });
 
 module.exports = router;
